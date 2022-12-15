69edbd4834314d729332ddb8f5ed148f4b9a378b
ntwigamartin <ntwiga.martin@gmail.com>
2022-12-15 16:49:40 +0300

    added db.json data & created 1st fetch

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 //load dom first
 document.addEventListener("DOMContentLoaded", (e)=>{
-
+    listHomes()
 })
+
+//General utilities
+const navBar = document.querySelector(".navBar")
+const mainDiv = document.querySelector(".main-container")
+const listDiv = document.querySelector(".homes-listing")
+const displayDiv = document.querySelector(".display-home-details")
+const addHomeDiv = document.querySelector(".add-homes")
+const ul = document.createElement("ul")
+const homesArray = "http://localhost:3000/homes"
+
+//Get homes
+function listHomes () {
+    fetch(homesArray)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data);
+    })
+}
