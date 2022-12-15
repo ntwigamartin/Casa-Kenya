4a120cc8a64dfb88cfa5de776c0a7649e1e9e014
ntwigamartin <ntwiga.martin@gmail.com>
2022-12-15 17:32:54 +0300

    displayed list of homes

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const listDiv = document.querySelector(".homes-listing")
 const displayDiv = document.querySelector(".display-home-details")
 const addHomeDiv = document.querySelector(".add-homes")
 const ul = document.createElement("ul")
+listDiv.appendChild(ul)
 const homesArray = "http://localhost:3000/homes"
 
 //Get homes
@@ -18,5 +19,11 @@ function listHomes () {
     .then(res=>res.json())
     .then(data=>{
         console.log(data);
+        data.forEach(home => {
+            let p = document.createElement("p")
+            p.innerHTML = home.name
+            ul.appendChild(p)
+
+        });
     })
 }
